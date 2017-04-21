7fa6e0532c4ca012ff0f813428666cf20f35dfab
Abdul Munim kazia <munim@browserstack.com>
2017-04-21 17:22:06 +0530

    moving to github

---
+++ index.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="Abdul Munim Kazia">
+		<meta name="author" content="Abdul Munim Kazia">
+		<title>Abdul Munim Kazia</title>
+	<link href='http://fonts.googleapis.com/css?family=Noticia+Text|Signika+Negative' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="styles.css">
+		<!--[if lt IE 9]>
+      	<script src="http://current.bootstrapcdn.com/js/html5.js"></script>
+    	<![endif]-->
+    </head>
+    <body>
+    	<header>
+    		<h1>Abdul Munim Kazia</h1>
+    		<h2>Software Engineer</h2>
+    	</header>
+        <aside>
+            <img src="http://2.gravatar.com/avatar/98a2a8a3bdc203ab221d3f5534d0b03f?size=200" alt="Abdul Munim Kazia">
+        </aside>
+    	<article>
+    		<p>Hello. I'm Munim. I am a Software Engineer. I like solving problems.</p>
+    		<p>I like building strong, scalable backends and server applications.</p>
+    		<p>I use a lot of tools and languages. I like learning new tools on the go, and picking the best tool for the job. But some of my favourites are javascript + nodejs, ruby, php, bash, c.</p>
+    		<p>Apart from web development, I am studying finance in my free time. I also like tinkering and developing on Android.</p>
+    		<p>I will be adding more content about my work here soon. I am in the process of redoing my old homepage. Feel free to get in touch with me in the meantime.</p>
+		</article>
+		<nav>
+            <ul>
+                <li><a href="mailto:mail@munimkazia.com">Email</a></li>
+			    <li><a href="http://twitter.com/munimkazia">Twitter</a></li>
+                <li><a href="http://github.com/munimkazia">Github</a></li>
+            </ul>
+		</nav>
+    </body>
+</html>
